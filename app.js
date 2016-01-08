93eeea685c46cd9b86bc08829c4d9cd7a22049de
snakeliwei <snakeliwei@gmail.com>
2016-01-08 14:45:11 +0800

    Update app.js

--- app.js
+++ app.js
@@ -39,36 +39,36 @@ app.use('/wechat', wechat(config, function (req, res, next) {
   if (message.FromUserName === 'diaosi') {
     // 回复屌丝(普通回复)
     res.reply('hehe');
-  } else if (message.FromUserName === 'text') {
-    //你也可以这样回复text类型的信息
-    res.reply({
-      content: 'text object',
-      type: 'text'
-    });
-  } else if (message.FromUserName === 'hehe') {
-    // 回复一段音乐
-    res.reply({
-      type: "music",
-      content: {
-        title: "来段音乐吧",
-        description: "一无所有",
-        musicUrl: "http://mp3.com/xx.mp3",
-        hqMusicUrl: "http://mp3.com/xx.mp3",
-        thumbMediaId: "thisThumbMediaId"
-      }
-    });
-  } else {
-    // 回复高富帅(图文回复)
-    res.reply([
-      {
-        title: '最新blog文章',
-        description: 'lyndon\'s blog',
-        picurl: 'http://snakeliwei.github.io/images/nodejs.jpg',
-        url: 'http://snakeliwei.github.io/'
-      }
-    ]);
-  }
-}));
+    } else if (message.FromUserName === 'text') {
+      //你也可以这样回复text类型的信息
+      res.reply({
+        content: 'text object',
+        type: 'text'
+        });
+      } else if (message.FromUserName === 'hehe') {
+        // 回复一段音乐
+          res.reply({
+            type: "music",
+            content: {
+              title: "来段音乐吧",
+              description: "一无所有",
+              musicUrl: "http://mp3.com/xx.mp3",
+              hqMusicUrl: "http://mp3.com/xx.mp3",
+              thumbMediaId: "thisThumbMediaId"
+            }
+            });
+        } else {
+          // 回复高富帅(图文回复)
+          res.reply([
+              {
+                title: '最新blog文章',
+                description: 'lyndon\'s blog',
+                picurl: 'http://snakeliwei.github.io/images/nodejs.jpg',
+                url: 'http://snakeliwei.github.io/'
+              }
+            ]);
+          }
+      }));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
